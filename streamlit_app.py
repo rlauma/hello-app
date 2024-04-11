e9838cd3ec376941e9541a10e5a5035fbf2bf736
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 16:30:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,60 @@
 import streamlit as st
-import streamlit.components.v1 as components
-import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.dummy import DummyRegressor
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error as mse
-from math import sqrt
-import matplotlib.pyplot as plt
-import time
-import requests
-import json
+st.title("Streamlit Tutorial App")
+st.write("This is my new app")
+button1 = st.button("Click Me")
+if button1:
+    st.write("This is some text.")
 
-st.title('Real Estate Predictions')
+st.header("Start of the Checkbox Section")
+like = st.checkbox("Do you like this app?")
+button2 = st.button("Submit")
+if button2:
+    if like:
+        st.write("Thanks. I like it too.")
+    else:
+        st.write("I'm sorry. You have bad tastes.")
+
+st.header("Start of the Radio Button Section")
+animal = st.radio("What animal is your favorite?", ("Lion", "Tiger", "Bear"))
+button3 = st.button("Submit Animal")
+if button3:
+    st.write(animal)
+    if animal == "Lion":
+        st.write("ROAR!")
+
+st.header("Start of the Selectbox Section")
+animal2 = st.selectbox("What animal is your favorite?", ("Lion", "Tiger", "Bear"))
+button4 = st.button("Submit Animal 2")
+if button4:
+    st.write(animal2)
+    if animal2 == "Lion":
+        st.write("ROAR!")
 
 
-def run_status():
-	latest_iteration = st.empty()
-	bar = st.progress(0)
-	for i in range(100):
-		latest_iteration.text(f'Percent Complete {i+1}')
-		bar.progress(i + 1)
-		time.sleep(0.1)
-		st.empty()
+st.header("Start of the Multiselect Section")
+options = st.multiselect("What animals do you like?",
+["Lion", "Tiger", "Bear"])
+button5 = st.button("Print Animals")
+if button5:
+    st.write(options)
 
-st.subheader('Multi Model Predictions')
 
-@st.cache
-def load_data():
-	df=pd.read_excel('data.xls')
-	df=df.drop(['country'],axis=1)
-	df=df[df['price']>0]
-	df.rename(columns={'statezip':'zip'}, inplace=True)
-	df['zip']=df['zip'].str.replace('WA','').astype(int)
-	df['floors']=df['floors'].astype(int)
-	df=df[df['bedrooms']>0]
-	df=df[df['bathrooms']>0]
-	return df
+st.header("Start of the Slider Section")
+epochs_num = st.slider("How many epochs?", 1,100, 10)
+if st.button("Slider Button"):
+    st.write(epochs_num)
 
-df=load_data()
+st.header("Start of the Text Input Section")
+user_text = st.text_input("What's your favorite movie?", "Star Wars Ep. IV")
+if st.button("Text Button"):
+    st.write(int(user_text))
+
+user_num = st.number_input("What's your favorite number?")
+if st.button("Number Button"):
+    st.write(user_num)
+
+def run_sentiment_analysis(txt):
+    st.write(f"Analysis Done. {txt}")
+
+txt = st.text_area('Text to analyze', '''It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
