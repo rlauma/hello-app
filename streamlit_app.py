0423a3cb91efbd83bde4adb3dfba6a22d3bc249e
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 19:47:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+# Create some sample data
+st.header("Triviālais nosaukums")
+data = { 
+    'Taukskābes nosaukums': ['palmitīnskābe (16:0)', 'stearīnskābe (18:0)', 'oleīnskābe (18:1n-9)', 'linolēnskābe (18:2n-6)', 'palmitoleīnskābe (16:1n-7)'],
+    'Ķīmiskā formula': ['C16H32O2', 'C18H36O2', 'C18H34O2', 'C18H32O2', 'C16H30O2'],
+    'Oglekļu atomu skaits': ['16', '18', '18', '18', '16'],
+    'Dubultsaite': ['', '', '1', '2', '1'],
+}
 
-
-
+# Convert the data to a DataFrame
+df = pd.DataFrame(data)
 
 # Display headers and radio button
 st.header("Dzīvnieku katalogs")
