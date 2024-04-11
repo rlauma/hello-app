bd833fd096615a4ab9cd587b18a5c98d7f03cf68
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 18:38:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.header("Triviālais nosaukums")
 data = {
     'Taukskābes nosaukums': ['Taukskābes nosaukums', 'Ķīmiskā formula', 'Oglekļu atomu skaits', 'Ķēžu garums', 'Ķēžu garums'],
-    'Ķīmiskā formula': [palmitīnskābe (16:0), stearīnskābe (18:0), oleīnskābe (18:1n-9), linolēnskābe (18:2n-6), palmitoleīnskābe (16:1n-7)],
+    'Ķīmiskā formula': ['palmitīnskābe (16:0)', 'stearīnskābe (18:0)', 'oleīnskābe (18:1n-9)', 'linolēnskābe (18:2n-6)', 'palmitoleīnskābe (16:1n-7)'],
     'Oglekļu atomu skaits': ['New York', 'London', 'Tokyo', 'Tokyo', 'Tokyo'],
 }
 
