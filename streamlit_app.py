47749e6faf1a16aa04be0dab01813ed231bd2a80
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 17:10:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,10 @@ button3 = st.button("Apstiprināt")
 if button3:
     st.write(animal)
     if animal == "Brūnais lācis":
+if button3:
         st.write("Ursus arctos")
         if animal == "Ziemeļu sikspārnis":
+if button3:
         st.write("Eptesicus nilssoni")
         if animal == "Eirāzijas āpsis":
         st.write("Meles meles")
