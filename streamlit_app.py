551a3a87d8e4cc757a0692d6ec4e4a9463a8e105
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 18:17:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 # Create some sample data
 data = {
-    'Triviālais nosaukums': ['Taukskābes nosaukums', 'Ķīmiskā formula', 'Oglekļu atomu skaits', 'Ķēžu garums'],
+    st.header("Triviālais nosaukums")
+    'Taukskābes nosaukums': ['Taukskābes nosaukums', 'Ķīmiskā formula', 'Oglekļu atomu skaits', 'Ķēžu garums'],
     'Age': [25, 30, 35, 30],
     'Location': ['New York', 'London', 'Tokyo', 'Tokyo']
 }
