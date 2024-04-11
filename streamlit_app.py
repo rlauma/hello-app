45b086d47d3c2a6ee282986774229638fe3bdfd5
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 17:16:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,18 @@ import streamlit as st
 st.header("Dzīvnieku katalogs")
 st.write("This is my new app")
 
+st.header("Vispārīgie dati")
+animal = st.radio("Dzīvnieku nosaukums", ("Brūnais lācis", "Ziemeļu sikspārnis", "Eirāzijas āpsis"))
+button3 = st.button("Apstiprināt")
+if button3:
+    st.write(animal)
+    if animal == "Brūnais lācis":
+if button3:
+        st.write("Ursus arctos")
+        if animal == "Ziemeļu sikspārnis":
+if button3:
+        st.write("Eptesicus nilssoni")
+        if animal == "Eirāzijas āpsis":
+        st.write("Meles meles")
+
+
