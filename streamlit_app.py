194d12184c1c421790954deb418b10b9ecb2c6dc
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 17:45:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
+import streamlit as st
+import pandas as pd
+from pydataset import data
 
+st.title('🎈 pydataset')
 
 selected_data = st.sidebar.selectbox('Select a dataset', data().dataset_id)
 title_data = data()[ data()['dataset_id'] == selected_data]['title']
