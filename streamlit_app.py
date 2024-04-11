18ebed081a5ec88dcc21859d6f17be3197219e0a
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 18:05:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import pandas as pd
 # Create some sample data
 data = {
     'Triviālais nosaukums': ['Taukskābes nosaukums', 'Ķīmiskā formula', 'Oglekļu atomu skaits', 'Ķēžu garums'],
-    'Age': [25, 30, 35],
-    'Location': ['New York', 'London', 'Tokyo']
+    'Age': [25, 30, 35, 30],
+    'Location': ['New York', 'London', 'Tokyo', 'ss']
 }
 
 # Convert the data to a DataFrame
