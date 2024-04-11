936f9a27165abe1c28b2165604535f996d1e0163
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-11 17:11:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,6 @@ st.title("Dzīvnieku katalogs")
 
 
 st.header("Vispārīgie dati")
-animal = st.radio("Dzīvnieku nosaukums", ("Brūnais lācis", "Ziemeļu sikspārnis", "Eirāzijas āpsis"))
-button3 = st.button("Apstiprināt")
-if button3:
-    st.write(animal)
-    if animal == "Brūnais lācis":
-if button3:
-        st.write("Ursus arctos")
-        if animal == "Ziemeļu sikspārnis":
-if button3:
-        st.write("Eptesicus nilssoni")
-        if animal == "Eirāzijas āpsis":
-        st.write("Meles meles")
-
-
-
-
-
-
-
-
-
-
-st.header("Start of the Selectbox Section")
-animal2 = st.selectbox("What animal is your favorite?", ("Lion", "Tiger", "Bear"))
-button4 = st.button("Submit Animal 2")
-if button4:
-    st.write(animal2)
-    if animal2 == "Lion":
-        st.write("ROAR!")
 
 
 
