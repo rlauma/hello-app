1609a22779779f32d8b558792a8fb1c75528bf39
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-12 10:06:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import streamlit as st
 
 # Create some sample data
 st.header("Triviālais nosaukums")
@@ -7,7 +8,7 @@ data = {
     'Taukskābes nosaukums': ['palmitīnskābe (16:0)', 'stearīnskābe (18:0)', 'oleīnskābe (18:1n-9)', 'linolēnskābe (18:2n-6)', 'palmitoleīnskābe (16:1n-7)'],
     'Ķīmiskā formula': ['C16H32O2', 'C18H36O2', 'C18H34O2', 'C18H32O2', 'C16H30O2'],
     'Oglekļu atomu skaits': ['16', '18', '18', '18', '16'],
-    'Dubultsaite': ['', '', '1', '2', '1'],
+    'Dubultsaišu skaits': ['', '', '1', '2', '1'],
 }
 
 # Convert the data to a DataFrame
