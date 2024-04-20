f2e236f368060c9f4bea6ebe9bc901bbf07ff233
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-20 21:44:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# Read data from the Google Sheets document
-google_sheets_url = "https://docs.google.com/spreadsheets/d/1OXzGj1jhVuzCmnRkYV8v8MqisPmKkz2MDIB8vDxeVrc/export?format=xlsx"
-df = pd.read_excel(google_sheets_url, sheet_name="Ūdensvads")
 
-# Display the DataFrame
-st.write(df)
 
 # Create some sample data for the brown bear
 data_brown_bear = { 
