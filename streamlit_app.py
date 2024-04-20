c34d44490dba1d7961c6b9dd3be1112ed205f451
rlauma <166692730+rlauma@users.noreply.github.com>
2024-04-20 21:44:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Read data from the Google Sheets document
 google_sheets_url = "https://docs.google.com/spreadsheets/d/1OXzGj1jhVuzCmnRkYV8v8MqisPmKkz2MDIB8vDxeVrc/export?format=xlsx"
-df = pd.read_excel(google_sheets_url, sheet_name="Ūdensvads", usecols=[0], engine='openpyxl')
+df = pd.read_excel(google_sheets_url, sheet_name="Ūdensvads")
 
 # Display the DataFrame
 st.write(df)
